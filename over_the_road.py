ddc4663e7cb6898135d53408c40a793d2c3a22aa
gombost84 <tamas.gombos84@gmail.com>
2023-03-11 11:28:12 +0100

    Over the road. This one took a while.

--- over_the_road.py
+++ over_the_road.py
@@ -1,7 +1,2 @@
-a = 7
-b = 11
-
 def over_the_road(address, n):
-    print((address + ((n - (round(address / 2))) * 2) + 1) - (2 * round(address / 2)) + 2)
-
-over_the_road(a, b)
+    return (n * 2) - address + 1
