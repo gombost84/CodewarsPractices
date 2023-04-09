2c362ab35e149f8f61840de09610362a27d76427
gombost84 <tamas.gombos84@gmail.com>
2023-04-09 18:47:00 +0200

    Valid Parentheses

---
+++ Python/valid_parentheses.py
@@ -0,0 +1,10 @@
+def valid_parentheses(s):
+    stack = []
+    for c in s:
+        if c == "(":
+            stack.append(c)
+        elif c == ")":
+            if not stack:
+                return False
+            stack.pop()
+    return not stack
